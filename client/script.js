e5e4116721b84855989ab20a4c400af9e2022534
Akash-Kharat <akashkharat1339@gmail.com>
2022-12-30 22:57:28 +0530

    done

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) =>{
   loader(messageDiv);
   
   //fetch data from the server
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://codex-lhtd.onrender.com/',{
     method:'POST',
     headers: {
       'Content-type' : "application/json"
