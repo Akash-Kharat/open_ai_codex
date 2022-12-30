0ecf8fde4fca35a99f67cfc52802819da2db55c7
Akash_Kharat <akashkharat1339@gmail.com>
2022-12-30 18:32:56 +0530

    ignore

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import {Configuration, OpenAIApi} from "openai";
 dotenv.config();
 
 const configuration = new Configuration({apiKey: process.env.OPENAI_API_KEY});
-
+console.log(configuration)
 const openai = new OpenAIApi(configuration);
 
 const app = express();
