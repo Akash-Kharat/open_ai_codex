63857ea49a052d7cbab16944d72e00b771165554
Akash-Kharat <akashkharat1339@gmail.com>
2023-01-07 22:34:19 +0530

    done

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.post('/', async (req, res) => {
           model: "text-davinci-003",
           prompt: `${prompt}`,
           temperature: 0.7,
-          max_tokens: 2991,
+          max_tokens: 3999,
           top_p: 0.5,
           frequency_penalty: 0,
           presence_penalty: 0,
